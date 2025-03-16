cae791dbdb710b73a51938ea3c08ef3be3812bd2
chan <truemindy@gmail.com>
2025-03-16 20:34:50 +0900

    줄바꿈 조정, 커밍쑨 메소드 추가

--- web/chatbot_menu.php
+++ web/chatbot_menu.php
@@ -8,6 +8,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Kaisei+Tokumin:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Korean:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&display=swap" rel="stylesheet">
+    <script type='text/javascript' src='/assets/sweetalert/sweetalert2.all.min.js'></script>
+    <script>
+        let fn_coming_soon = () => {
+            Swal.fire("Coming Soon", "서비스 준비 중입니다.", "info");
+        }
+    </script>
 </head>
 
 <body>
@@ -32,7 +38,6 @@
                             <div id='card_name' class='card_name'>
                                 SNS전문 마케터</div>
                             <img id='edit-marker' class='edit-marker' src='../img/sns.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
                             매력적인 SNS 홍보 글 생성</div>
@@ -42,61 +47,63 @@
                     <div id='card_contents' class='card_contents'>
                         <div id='card_name_icon' class='card_name_icon'>
                             <div id='card_name' class='card_name'>
-                                리뷰 마법사</div>
+                                리뷰 마법사
+                            </div>
                             <img id='edit-marker' class='edit-marker' src='../img/review.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
-                            자연스러운 고객 리뷰 작성</div>
+                            자연스러운 고객 리뷰 작성
+                        </div>
                     </div>
                 </div>
-                <div id='chat_card' class='chat_card'>
+                <div id='chat_card' class='chat_card' onclick="fn_coming_soon()">
                     <div id='card_contents' class='card_contents'>
                         <div id='card_name_icon' class='card_name_icon'>
                             <div id='card_name' class='card_name'>
                                 라고사나 Q&A</div>
                             <img id='edit-marker' class='edit-marker' src='../img/qna.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
-                            이럴 땐, 이렇게 사용하세요</div>
+                            이럴 땐, 이렇게 사용하세요
+                        </div>
                     </div>
                 </div>
             </div>
             <div id='cards' class='cards'>
-                <div id='chat_card' class='chat_card'>
+                <div id='chat_card' class='chat_card' onclick="fn_coming_soon()">
                     <div id='card_contents' class='card_contents'>
                         <div id='card_name_icon' class='card_name_icon'>
                             <div id='card_name' class='card_name'>
                                 이벤트 플래너</div>
                             <img id='edit-marker' class='edit-marker' src='../img/planner.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
                             고객 맞춤 이벤트 기획과<br>
-                            홍보 전략까지 추천</div>
+                            홍보 전략까지 추천
+                        </div>
                     </div>
                 </div>
-                <div id='chat_card' class='chat_card'>
+                <div id='chat_card' class='chat_card' onclick="fn_coming_soon()">
                     <div id='card_contents' class='card_contents'>
                         <div id='card_name_icon' class='card_name_icon'>
                             <div id='card_name' class='card_name'>
-                                시술 패키징 마법사</div>
+                                시술 패키징 마법사
+                            </div>
                             <img id='edit-marker' class='edit-marker' src='../img/wizard.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
                             고객 맞춤 시술 구성 및<br>
-                            홍보 문구 작성</div>
+                            홍보 문구 작성
+                        </div>
                     </div>
                 </div>
-                <div id='chat_card' class='chat_card'>
+                <div id='chat_card' class='chat_card' onclick="fn_coming_soon()">
                     <div id='card_contents' class='card_contents'>
                         <div id='card_name_icon' class='card_name_icon'>
                             <div id='card_name' class='card_name'>
-                                Daily SNS</div>
+                                Daily SNS
+                            </div>
                             <img id='edit-marker' class='edit-marker' src='../img/daily.svg' />
-                            </img>
                         </div>
                         <div id='card_description' class='card_description'>
                             매일매일 습관처럼<br>SNS 포스팅하기
